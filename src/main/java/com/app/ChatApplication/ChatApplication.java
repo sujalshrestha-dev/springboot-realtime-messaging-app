b77ba41bb7653506edff1209f06491cd4a0896b1
Sujal Shrestha <shresthasujal2021@gmail.com>
2026-05-27 16:35:12 +0545

    frontend first error

--- src/main/java/com/app/ChatApplication/ChatApplication.java
+++ src/main/java/com/app/ChatApplication/ChatApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChatApplication {
+public class  ChatApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatApplication.class, args);
